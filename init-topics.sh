58019e47195ec01aaa1409325ab60f2c2a1d18e9
Vinicius Escame <v.escame@gmail.com>
2021-04-21 14:27:03 -0300

    Fix kafka topic names

--- init-topics.sh
+++ init-topics.sh
@@ -5,10 +5,9 @@ container='kafka'
 docker-compose exec $container kafka-topics \
   --create --zookeeper zookeeper:2181 \
   --replication-factor 1 --partitions 1 \
-  --topic streaming.ecommerce.checkout.created --if-not-exists
+  --topic streaming.yerbamate.checkout.created --if-not-exists
 
 docker-compose exec $container kafka-topics \
   --create --zookeeper zookeeper:2181 \
   --replication-factor 1 --partitions 1 \
-  --topic streaming.ecommerce.payment.paid --if-not-exists
-
+  --topic streaming.yerbamate.payment.verified --if-not-exists
